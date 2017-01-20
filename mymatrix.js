fa3e9c9ac9ea5b6e59f7233b0086cef9ea0509ec
Rory O'Connor <7roconnor@stpatrick.org>
2017-01-20 13:56:03 -0600

    Add files via upload

--- mymatrix.js
+++ mymatrix.js
@@ -4,18 +4,23 @@ const Matrix = require("matrix-js");
 /*Example code creating two new matricies and then multiplying them.*/
 let M = Matrix([[1,1],[1,1]]);
 let N = Matrix([[2,3],[3,5]]);
-//console.log(M.prod(N));
 
-//Draft your code below this line to answer the questions.
-//You can 'comment out' th  example above by placing double slashes before them.
 
 let A = Matrix([
-  [0,1,1,0,0,1],
-  [1,0,1,0,0,1],
-  [1,1,0,1,1,1],
-  [0,0,1,0,0,0],
-  [0,0,1,0,0,0],
-  [1,1,1,0,0,0]
+    [0,1,1,0,0,1],
+    [1,0,1,0,0,1],
+    [1,1,0,1,1,1],
+    [0,0,1,0,0,0],
+    [0,0,1,0,0,0],
+    [1,1,1,0,0,0]
 ]);
 
-console.log(A.prod(A));
+//Draft your code below this line to answer the questions.
+//You can 'comment out' theexample above by placing double slashes before them.
+
+
+let squared = Matrix(A.prod(A));
+let cubed = Matrix(squared.prod(A));
+
+console.log(squared());
+console.log(cubed()); 
